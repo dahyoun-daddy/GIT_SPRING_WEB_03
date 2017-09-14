d350d8565743115eab40973061202af7d77c31ca
SonSB <ssb90225@naver.com>
2017-09-14 14:02:44 +0900

    테스트:성범

---
+++ Hello_Git03/src/com/sist/g3/TestSB.java
@@ -0,0 +1,11 @@
+package com.sist.g3;
+
+public class TestSB {
+
+	public static void main(String[] args) {
+		System.out.println("손성범입니다.");
+		System.out.println("테스트를 시작합니다.");
+		System.out.println("커밋좀 되게 해주세요 ㅜㅜ");
+	}
+
+}
