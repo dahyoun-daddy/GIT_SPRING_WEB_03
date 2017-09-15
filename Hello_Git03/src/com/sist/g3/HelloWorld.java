b6f96f3c9e95c2db18f52c825b471e2a2428e2eb
jeongin <pretty11n@naver.com>
2017-09-15 09:22:25 +0900

    helloworld-1

--- Hello_Git03/src/com/sist/g3/HelloWorld.java
+++ Hello_Git03/src/com/sist/g3/HelloWorld.java
@@ -3,8 +3,7 @@ package com.sist.g3;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//
+		System.out.println("HelloWorld-1-1-1");
 	}
 
 }
